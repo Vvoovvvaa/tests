d82cfd0e6a1b16007ada41343467898ee1970fac
Vova-Markosyan <vovamarkosyan074@gmail.com>
2024-12-23 23:55:07 +0400

    Final Update

--- XO.py
+++ XO.py
@@ -1,137 +1,82 @@
 import random
 
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+board = [' '] * 10
 player = 1
-Win = 1
-Draw = -1
-Running = 0
-Game = Running
-select = 'X'
+Game = 0  
 
-def DrawBoard():
-    print(" %c | %c | %c " % (board[1], board[2], board[3]))
+def draw_board():
+    print(f" {board[1]} | {board[2]} | {board[3]} ")
     print("___|___|___")
-    print(" %c | %c | %c " % (board[4], board[5], board[6]))
+    print(f" {board[4]} | {board[5]} | {board[6]} ")
     print("___|___|___")
-    print(" %c | %c | %c " % (board[7], board[8], board[9]))
-    print("   |   |")
+    print(f" {board[7]} | {board[8]} | {board[9]} ")
+    print("   |   |   ")
 
-def logika():
+def check_winner():
     global Game
-    if (board[1] == board[2] == board[3] and board[1] != ' ') or \
-       (board[4] == board[5] == board[6] and board[4] != ' ') or \
-       (board[7] == board[8] == board[9] and board[7] != ' ') or \
-       (board[1] == board[4] == board[7] and board[1] != ' ') or \
-       (board[2] == board[5] == board[8] and board[2] != ' ') or \
-       (board[3] == board[6] == board[9] and board[3] != ' ') or \
-       (board[1] == board[5] == board[9] and board[5] != ' ') or \
-       (board[3] == board[5] == board[7] and board[5] != ' '):
-        Game = Win
-    elif ' ' not in board[1:]:
-        Game = Draw
-    else:
-        Game = Running
+    win_positions = [
+        [1, 2, 3], [4, 5, 6], [7, 8, 9],  
+        [1, 4, 7], [2, 5, 8], [3, 6, 9],  
+        [1, 5, 9], [3, 5, 7]              
+    ]
+    for pos in win_positions:
+        if board[pos[0]] == board[pos[1]] == board[pos[2]] and board[pos[0]] != ' ':
+            Game = 1
+            return
+    if ' ' not in board[1:]:
+        Game = -1
 
-def intro():
-    print("Hello! Welcome to X O game!")
-    print("I'm sure you know the rules")
-    print("""
-    Select a game mode:
-    1. Player vs Player
-    2. Player vs Robot
-    """)
-    choice = input("Enter 1 or 2 to select a mode: ")
-    if choice == '1':
-        return 'player_vs_player'
-    elif choice == '2':
-        return 'player_vs_robot'
-    else:
-        print("Invalid selection, defaulting to Player vs Player.")
-        return 'player_vs_player'
+def is_position_free(pos):
+    return board[pos] == ' '
 
-def check_position(x):
-    return board[x] == ' '
-
-def start_game():
-    global player, select, Game, board
-    while Game == Running:
-        DrawBoard()
-        if player % 2 != 0:
-            print("Player 1's turn (X)")
-            select = 'X'
-        else:
-            print("Player 2's turn (O)")
-            select = 'O'
-
+def player_move():
+    while True:
         try:
-            choice = int(input("Enter the position between [1-9]: "))
-            if choice < 1 or choice > 9:
-                print("Invalid input! Please enter a number between 1 and 9.")
-                continue
+            pos = int(input("Enter a position (1-9): "))
+            if pos < 1 or pos > 9:
+                print("Invalid position! Choose between 1 and 9.")
+            elif not is_position_free(pos):
+                print("Position already taken. Try another one.")
+            else:
+                return pos
         except ValueError:
-            print("Invalid input! Please enter a valid number.")
-            continue
+            print("Invalid input! Enter a number between 1 and 9.")
 
-        if check_position(choice):
-            board[choice] = select
-            player += 1
-            logika()
+def robot_move():
+    available_positions = [i for i in range(1, 10) if is_position_free(i)]
+    return random.choice(available_positions)
+
+def play_game(is_robot):
+    global player, Game, board
+    while Game == 0:
+        draw_board()
+        print(f"Player {player}'s turn ({'X' if player == 1 else 'O'})")
+        if is_robot and player == 2:
+            pos = robot_move()
+            print(f"Robot chooses position {pos}")
         else:
-            print("Position already taken, try again.")
-            
-        if Game == Win:
-            DrawBoard()
-            print(f"Player {player % 2 + 1} wins!")
-            break
-        elif Game == Draw:
-            DrawBoard()
+            pos = player_move()
+        board[pos] = 'X' if player == 1 else 'O'
+        check_winner()
+        if Game == 1:
+            draw_board()
+            print(f"Player {player} wins!")
+        elif Game == -1:
+            draw_board()
             print("It's a draw!")
-            break
-
-def vsrobot():
-    global player, select, Game, board
-    while Game == Running:
-        DrawBoard()
-        if player % 2 != 0:
-            print("Player 1's turn (X)")
-            select = 'X'
-            try:
-                choice = int(input("Enter the position between [1-9]: "))
-                if choice < 1 or choice > 9:
-                    print("Invalid input! Please enter a number between 1 and 9.")
-                    continue
-            except ValueError:
-                print("Invalid input! Please enter a valid number.")
-                continue
-        else:
-            print("Player 2's turn (O) - Robot")
-            select = 'O'
-            available_positions = [i for i in range(1, 10) if board[i] == ' ']
-            choice = random.choice(available_positions)
-            print(f"Robot chooses position {choice}")
-
-        if check_position(choice):
-            board[choice] = select
-            player += 1
-            logika()
-        else:
-            print("Position already taken, try again.")
-            
-        if Game == Win:
-            DrawBoard()
-            print(f"Player {player % 2 + 1} wins!")
-            break
-        elif Game == Draw:
-            DrawBoard()
-            print("It's a draw!")
-            break
+        player = 3 - player 
 
 def main():
-    game_mode = intro()
-    if game_mode == 'player_vs_player':
-        start_game()
-    elif game_mode == 'player_vs_robot':
-        vsrobot()
+    print("Welcome to Tic Tac Toe!")
+    print("Choose game mode:")
+    print("1. Player vs Player")
+    print("2. Player vs Robot")
+    while True:
+        mode = input("Enter 1 or 2: ")
+        if mode in ['1', '2']:
+            break
+        print("Invalid choice. Enter 1 or 2.")
+    play_game(is_robot=(mode == '2'))
 
 if __name__ == "__main__":
     main()
